a296ccef4c20229e54748897ae7e761c2f2a0c66
Oscar Knagg <oscar@knagg.co.uk>
2018-09-01 16:15:16 +0100

    Added unit test for whiten() util function

--- tests.py
+++ tests.py
@@ -1,10 +1,23 @@
 import unittest
+import torch
+import soundfile as sf
+import numpy as np
+
 from utils import whiten
+from config import PATH
 
 
 class TestWhitening(unittest.TestCase):
     def test_whitening(self):
-        # Create some test audio
-        # Or load some test audio
-        # Run the whitening and test that the RMS and mean are what is expected
-        pass
+        desired_rms = 0.038021
+
+        test_data, sample_rate = sf.read(PATH + '/data/whitening_test_audio.flac')
+        test_data = np.stack([test_data]*2)
+
+        whitened = whiten(torch.from_numpy(test_data),desired_rms)
+
+        # Mean correct
+        self.assert_(np.isclose(whitened.mean().item(),0))
+
+        # RMS correct
+        self.assert_(np.isclose(np.sqrt(np.power(whitened[0,:], 2).mean()).item(), desired_rms))
